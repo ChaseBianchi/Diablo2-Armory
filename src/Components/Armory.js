525896166a71db14978cf368f370318c002efdd6
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2021-08-24 19:21:36 -0400

    three panel layout, state/data structure changes, transitions
    3 panels now, transitions added on character selection and weapon selection. Handling opacity in context api for the transitions. Data structure of weapons changed to match PD2 data structure.

--- src/Components/Armory.js
+++ src/Components/Armory.js
@@ -3,28 +3,28 @@ import { UserContext } from '../Contexts'
 import { weaponlist } from '../weapons'
 import defaultimg from '../media/default.png'
 
+const styles = {
+    transition: 'opacity 1s ease-in-out',
+    // transitionTimingFunction: 'Fast Out, Linear In'
+}
 
 function Armory() {
+    
+    const {toon, weapon, damage, setWeapAndPanels, leftPanel, midPanel, rightPanel } = useContext(UserContext)
 
-    const {toon, weapon, setWeapon} = useContext(UserContext)
+
     const handleWeaponSelect = evt =>{
         console.log(evt.target)
         const ele = evt.target.getAttribute('tag')
-        setWeapon({name: evt.target.value, tag: ele})
+        setWeapAndPanels({name: evt.target.value, tag: ele})
+        
     }
-    
-    const handleWeaponDropdown = evt =>{
-        console.log('setWeaponName ', setWeapon)
-        setWeapon('test2')
-        console.log(weapon)
-    }
-    return (
-        <div className={`armory-box ${toon ? '' : 'hidden'}`}>
-            <div className={`left-armory panel ${weapon.name ? '' : 'hidden'}`}>
-                {weapon.name}
-                {weapon.tag}
+        return (
+        <div className={`armory-box`}>
+            <div style={{...styles, opacity: leftPanel,  }} className={`left-armory panel`}>
+                <h2>Weapon</h2>
             </div>
-            <div className="right-armory panel">
+            <div style={{...styles, opacity: midPanel }} className="middle-armory panel">
                 <div className="weapon-select">
                 <select value={weapon.name} onChange={handleWeaponSelect}>
                     <option value=''>Select Weapon</option>
@@ -33,9 +33,34 @@ function Armory() {
                     })}
                 </select>
                 <div className='weapon-image-div'>
-                    <img src='/media/default.png' alt="weapon" className="weapon-image" />
+                    <img style={weapon.name ? {border: '3px solid #A1A1A1'} : {}} src={weapon.name ? `/media/${weapon.name.toLowerCase().replace(/\s+/g, '')}.png` : '/media/default.png'} 
+                    alt="weapon" className="weapon-image" 
+                    />
+                </div>
+                <div className="damage-box">
+                    <div className="left-damage">
+                    <h1>DAMAGE </h1>
+                        <h3 className='phys dmg-type'>physical</h3>
+                        <h3 className="fire dmg-type">fire</h3>
+                        <h3 className="light dmg-type">lightning</h3>
+                        <h3 className="cold dmg-type">cold</h3>
+                        <h3 className="poison dmg-type">poison</h3>
+                    </div>
+                    <div className="right-damage">
+                    <h1>{damage.total}</h1>
+                        <h3 className={`phys dmg-type `}>{damage.physical}</h3>
+                        <h3 className="fire dmg-type">{damage.fire}</h3>
+                        <h3 className="light dmg-type">{damage.lightning}</h3>
+                        <h3 className="cold dmg-type">{damage.cold}</h3>
+                        <h3 className="poison dmg-type">{damage.poison}</h3>
+                    </div>
+                
                 </div>
                 </div>
+            </div>
+            <div style={{...styles, opacity: rightPanel,  }} className={`right-armory panel`}>
+                <h2>Character</h2>
+                
             </div>
         </div>
     )
--- src/Contexts/index.js
+++ src/Contexts/index.js
@@ -4,10 +4,17 @@ export const UserContext = createContext(
     {
         toon: '',
         weapon: {iLevel: 0, name: '', quality: '', type: '', isEthereal: false, isRuneword: false, runeword: '', isRune: false, socketed: [], stats: []},
-        setToon: ()=>{},
-        setWeapon: ()=>{},
+        damage: 0,
+        leftPanel: '0',
+        midPanel: '0',
+        rightPanel: '0',
+        toggleLeftPanel: ()=>{},
+        toggleMidPanel: ()=>{},
+        toggleRightPanel: ()=>{},
+        setWeapAndPanels: ()=>{},
         resetAll: ()=>{},
-
+        setWeapon: ()=>{},
+        setToon: ()=>{},
     }
 );
 
@@ -15,14 +22,42 @@ export const UserContextProvider = props =>{
     const initState = {
         toon: '',
         weapon: {iLevel: 0, name: '', quality: '', type: '', isEthereal: false, isRuneword: false, runeword: '', isRune: false, socketed: [], stats: []},
+        damage: {total: 0, physical: 0, fire: 0, cold: 0, lightning: 0, poison: 0},
+        leftPanel: '0',
+        midPanel: '0',
+        rightPanel: '0',
+        toggleLeftPanel: ()=>{},
+        toggleMidPanel: ()=>{},
+        toggleRightPanel: ()=>{},
+        setWeapAndPanels: ()=>{},
+        resetAll: ()=>{},
+        setWeapon: ()=>{},
+        setToon: ()=>{},
+        
     }
     const [userState, setUserState] = useState(initState)
     function setToon(toon){
-        const newState = {...userState, toon: toon}
+        const newState = {...userState, midPanel: '100', toon: toon}
         setUserState(newState)
     }
     function setWeapon(weapon){
         const newState = {...userState, weapon: {...userState.weapon, ...weapon}}
+        setUserState(newState)
+    }
+    function toggleLeftPanel(){
+        const newState = {...userState, leftPanel: userState.leftPanel==='0' ? '100' : '0'}
+        setUserState(newState)
+    }
+    function toggleMidPanel(){
+        const newState = {...userState, midPanel: userState.midPanel==='0' ? '100' : '0'}
+        setUserState(newState)
+    }
+    function toggleRightPanel(){
+        const newState = {...userState, rightPanel: userState.rightPanel==='0' ? '100' : '0'}
+        setUserState(newState)
+    }
+    function setWeapAndPanels(weapon){
+        const newState = {...userState, leftPanel: '100', rightPanel: '100', weapon: {...userState.weapon, ...weapon}}
         setUserState(newState)
     }
     function resetUser(){
@@ -30,5 +65,5 @@ export const UserContextProvider = props =>{
     }
     
     
-    return <UserContext.Provider value={{...userState, setWeapon, setToon, resetUser}} >{props.children}</UserContext.Provider>
+    return <UserContext.Provider value={{...userState, setWeapon, setToon, resetUser, toggleLeftPanel, toggleMidPanel, toggleRightPanel, setWeapAndPanels}} >{props.children}</UserContext.Provider>
 }
